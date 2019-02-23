f8167627d47365438e5c3dc6d9516589c126cd4d
sujan-de <sujandey.dey@gmail.com>
2019-02-23 16:17:40 +0530

    Devise integrated with authorization (#1)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
+    #devise_parameter_sanitizer.for(:sign_up) << :name
+    #devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username])
+  end
 end
